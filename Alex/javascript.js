1172d9da525e782f77939d562a504ad8e2c69b9c
ice_cube <a_aumayr@outlook.com>
2022-05-25 13:42:32 +0200

    finish all tasks

--- Alex/javascript.js
+++ Alex/javascript.js
@@ -83,3 +83,29 @@ console.log(multi_array[4][2]);
 console.log(multi_array[5][3]);
 console.log(multi_array[2][3]);
 console.log(multi_array[2][1]);
+
+var long_string = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.      $A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up"
+
+var long_string_array = long_string.split("$");
+
+console.table(long_string_array);
+
+long_string_array = long_string_array.join(" ");
+document.write("<br>");
+document.write("<br>");
+
+document.write(long_string_array);
+
+var name_array = ["Maria", "Mona", "Daniela", "Stefan", "Alex"];
+
+name_array.splice(2,1);
+console.log(name_array)
+
+var name1 = name_array.pop();
+var name2 = name_array.pop();
+name_array.pop();
+name_array.push(name2);
+name_array.push(name1);
+
+console.log(name_array);
+
